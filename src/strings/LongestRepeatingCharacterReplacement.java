4aee19fff153f54e8957b34ddc8f6fd1ca3502b2
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-07-19 18:19:42 +0530

    longest repeating character replacement

---
+++ src/strings/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,26 @@
+package strings;
+
+public class LongestRepeatingCharacterReplacement {
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("AAAABAAAA", 1));
+    }
+
+    public static int characterReplacement(String s, int k) {
+        int left = 0, right = 0;
+        int maxLen = Integer.MIN_VALUE;
+        int[] arr = new int[26];
+        int freqCharCount = Integer.MIN_VALUE;
+        while (right < s.length()) {
+            arr[s.charAt(right) - 'A']++;
+            freqCharCount = Math.max(freqCharCount, arr[s.charAt(right) - 'A']);
+            //contract left
+            if (right - left + 1 - freqCharCount > k) {
+                arr[s.charAt(left) - 'A']--;
+                left++;
+            }
+            maxLen = Math.max(right - left + 1, maxLen); // maintain max window
+            right++; // expand right
+        }
+        return maxLen;
+    }
+}
